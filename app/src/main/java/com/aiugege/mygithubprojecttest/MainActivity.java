ca3463afec6901b9224fb799038a0146c9308dcb
Leeky <82136450@qq.com>
2017-09-08 16:52:04 +0800

    Update MainActivity.java

--- app/src/main/java/com/aiugege/mygithubprojecttest/MainActivity.java
+++ app/src/main/java/com/aiugege/mygithubprojecttest/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Test 2017.09.08
+        // Test 2017.09.08 16:51
         setContentView(R.layout.activity_main);
     }
 }
